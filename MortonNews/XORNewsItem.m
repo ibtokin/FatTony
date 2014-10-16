80036cc0d50b315e2806eca68c332de807e238f9
Adan <adanasandoval@gmail.com>
2014-10-16 11:06:48 -0500

    Added comments about MVC

--- MortonNews/XORNewsItem.m
+++ MortonNews/XORNewsItem.m
@@ -3,8 +3,14 @@
 //  MortonNews
 //
 //  Created by Adan Sandoval on 8/30/14.
-//  Copyright (c) 2014 XOR Development, LLC. All rights reserved.
+//  Copyright (c) 2014 XOR Development, LLC. All rights reversed.
 //
+/*
+Each news item will be encapsulated in an object so that we don't have to decode JSON everywhere. This helps make our tables more easily extendable in the future. 
+
+According to Apple:
+
+"Model objects represent special knowledge and expertise. They hold an application’s data and define the logic that manipulates that data. A well-designed MVC application has all its important data encapsulated in model objects.... they represent knowledge and expertise related to a specific problem domain, they tend to be reusable."*/
 
 #import "XORNewsItem.h"
 @implementation XORNewsItem
